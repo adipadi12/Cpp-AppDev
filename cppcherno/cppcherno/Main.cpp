149d15922381dada131950d164a671fc78a342ea
adipadi12 <ajha4436@gmail.com>
2025-03-05 04:33:41 +0530

    cooked nothing

--- cppcherno/cppcherno/Main.cpp
+++ cppcherno/cppcherno/Main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <iostream> //pre-processor directive. including a file in program we gon be using later on
 using namespace std;
 
+void printLol(); //function prototype. allows code to be compiled even when function is called before declaration
+
 int main() {
-	cout << "Hello World";
+	//cout << "Hello \n World \n bacon"; //statement. ends with semicolon
+	//order of precedence
+	int x = 75;
+
+	cout << x % 10 <<endl;
+	printLol();
+}
+
+void printLol() {
+	cout << "NIGGGAAAAAAAAAAAA";
 }
