9b60b3464cad4f07753ef877926a92e77dbf0c96
adipadi12 <ajha4436@gmail.com>
2025-03-05 23:03:15 +0530

    basic setter and getter method. to access a private string inside a class

--- cppcherno/cppcherno/Main.cpp
+++ cppcherno/cppcherno/Main.cpp
@@ -12,18 +12,32 @@ int addNumbers(int x, int y) {
 }
 
 class AdisClass {
-	public: //anything put after this can be used outside of this class. caled an access specifier
-		void coolShit() {
-			cout << "failing tomorrow" << endl;
+	public:
+		void setName(string x) {
+			name = x; //setter method. made to access a private thing inside the class
+
 		}
+		string getName() {
+			return name;
+		}
+	private: //anything put after this can be used outside of this class. caled an access specifier
+		/*void coolShit() {
+			cout << "failing tomorrow" << endl;
+		}*/
+		string name;
 
 };
 
 int main() {
 	//printShit(69);
 	//cout << addNumbers(8, 10);
+
 	AdisClass adipadi; //named my class adipadi just like a variable
-	adipadi.coolShit();
+	//adipadi.coolShit(); //using a dot . we can access the functions in that class
+
+	adipadi.setName("poopy");
+	cout << adipadi.getName();
+	
 	return 0;
 }
 
