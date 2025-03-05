5c091353cba08f0b50b6a6df95c22d59883b6e19
adipadi12 <ajha4436@gmail.com>
2025-03-05 22:37:15 +0530

    q3. p = q means p now points to q. so p* = 2 means q = 2. hence answer is 0  2

--- cppcherno/cppcherno/Main.cpp
+++ cppcherno/cppcherno/Main.cpp
@@ -3,21 +3,18 @@ using namespace std;
 
 void printLol(); //function prototype. allows code to be compiled even when function is called before declaration
 
-void f1(int a, int b) {
-	int c;
-	c = a; a = b; b = c;
+#include <stdio.h>
+void f(int* p, int* q) {
+	p = q;
+	*p = 2;
 }
-void f2(int* a, int* b) {
-	int c;
-	c = *a; *a = *b; *b = c;
-}
+int i = 0, j = 1;
 int main() {
-	int a = 4, b = 5, c = 6;
-	f1(a, b);
-	f2(&b, &c);
-	printf("%d", c - a - b);
+	f(&i, &j);
+	printf("%d %d\n", i, j);
 	return 0;
 }
+
 
 void printLol() {
 	cout << "NIGGGAAAAAAAAAAAA" << endl;
