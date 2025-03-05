c8d0a142e6e98b592fcc99f592ccf73bd8d99044
adipadi12 <ajha4436@gmail.com>
2025-03-05 22:27:21 +0530

    q2. gives -5 because a and b swapped locally. so original values are same. but f2 swaps b and c

--- cppcherno/cppcherno/Main.cpp
+++ cppcherno/cppcherno/Main.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 void printLol(); //function prototype. allows code to be compiled even when function is called before declaration
 
+void f1(int a, int b) {
+	int c;
+	c = a; a = b; b = c;
+}
+void f2(int* a, int* b) {
+	int c;
+	c = *a; *a = *b; *b = c;
+}
 int main() {
-	//cout << "Hello \n World \n bacon"; //statement. ends with semicolon
-	//order of precedence
-	int x = 75;
-
-	printLol();
-
-	cout << x % 10 << endl;
-	char c[] = "GATE2011";
-	char* p = c;
-
-	printf("%s", p + p[3] - p[1]);
+	int a = 4, b = 5, c = 6;
+	f1(a, b);
+	f2(&b, &c);
+	printf("%d", c - a - b);
+	return 0;
 }
 
 void printLol() {
